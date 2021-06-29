222dfdb029ca10ba1b21e2eaecd44784c76b2c45
Lennart Weiss <weislenn@hu-berlin.de>
2021-06-29 21:49:56 +0200

    Fix queries

--- a4/BooleanQueryWordnet.java
+++ a4/BooleanQueryWordnet.java
@@ -9,6 +9,8 @@ import java.util.*;
 import doc.DocumentParser;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.DirectoryReader;
@@ -33,6 +35,10 @@ import static java.nio.charset.StandardCharsets.ISO_8859_1;
 public class BooleanQueryWordnet {
 
     HashMap<String, ArrayList<String>> synset;
+    HashMap<String, ArrayList<String>> synset_noun;
+    HashMap<String, ArrayList<String>> synset_verb;
+    HashMap<String, ArrayList<String>> synset_adj;
+    HashMap<String, ArrayList<String>> synset_adv;
     IndexSearcher indexSearcher;
     MultiFieldQueryParser queryParser;
 
@@ -42,8 +48,172 @@ public class BooleanQueryWordnet {
      * OF THE CONSTRUCTOR.
      */
     public BooleanQueryWordnet() {
-    
-    
+
+
+    }
+
+    public void enter_synset(HashMap<String, ArrayList<String>> set) {
+	ArrayList<String> list;
+	for(String key : set.keySet()) {
+	    if(synset.containsKey(key)) {
+		list = synset.get(key);
+	    } else {
+		list = new ArrayList<>();
+		synset.put(key, list);
+	    }
+	    for(String entry: set.get(key)) {
+		if((!entry.equals(key)) && !list.contains(entry))
+		    list.add(entry);
+	    }
+	}
+    }
+
+    public void merge_synsets() {
+	System.out.println("Merging synsets...");
+	enter_synset(synset_noun);
+	enter_synset(synset_verb);
+	enter_synset(synset_adj);
+	enter_synset(synset_adv);
+    }
+
+    public void skip_initial_lines_wordnet(BufferedReader reader) throws IOException {
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            if (line.contains("Princeton University and LICENSEE agrees to preserve same.")) {
+                return;
+            }
+        }
+
+    }
+
+    public void fill_synset(HashMap<String, ArrayList<String>> map, String syn,
+			    ArrayList<String> syns) {
+	ArrayList<String> list;
+	if (map.containsKey(syn)) {
+	    list = map.get(syn);
+	} else {
+	    list = new ArrayList<>();
+	    map.put(syn, list);
+	}
+	for (String syn2: syns) {
+	    if ((syn2.equals(syn)) || (list.contains(syn2)))
+		continue;
+	    list.add(syn2);
+	}
+    }
+
+    public boolean has_no_special_chars(String line) {
+	return (!line.contains("#"))
+	    && (!line.contains("@"))
+	    && (!line.contains("~"))
+	    && (!line.contains("!"))
+	    && (!line.contains("%"))
+	    && (!line.contains("="))
+	    && (!line.contains("+"))
+	    && (!line.contains(";"))
+	    && (!line.contains("-"))
+	    && (!line.contains("*"))
+	    && (!line.contains(">"))
+	    && (!line.contains("^"))
+	    && (!line.contains("$"))
+	    && (!line.contains("&"))
+	    && (!line.contains("<"))
+	    && (!line.contains("\\"))
+	    && (!line.contains("|"));
+    }
+
+    public void parse_exec_file(Path wordnetDir, String s) {
+	try (BufferedReader reader = Files.newBufferedReader(Path.of(wordnetDir + "/" + s + ".exc"), ISO_8859_1)) {
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+		ArrayList<String> syns = new ArrayList<>();
+		String[] syn_line = line.split(" ");
+		for(String syn : syn_line) {
+		    if(syn.contains("_"))
+			continue;
+		    syns.add(syn);
+		}
+		if(syns.size() < 2)
+		    continue;
+		for(String syn : syns) {
+		    switch(s) {
+		    case "noun":
+			fill_synset(synset_noun, syn, syns);
+			break;
+		    case "verb":
+			fill_synset(synset_verb, syn, syns);
+			break;
+		    case "adj":
+			fill_synset(synset_adj, syn, syns);
+			break;
+		    case "adv":
+			fill_synset(synset_adv, syn, syns);
+			break;
+		    }
+		}
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.exit(-1);
+	}
+    }
+	
+    public void parse_data_file(Path wordnetDir, String s) {
+	try (BufferedReader reader = Files.newBufferedReader(Path.of(wordnetDir + "/data." + s), ISO_8859_1)) {
+	    skip_initial_lines_wordnet(reader);
+		
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+		ArrayList<String> syns = new ArrayList<>();
+		String[] synset_line = line.split(" ");
+
+		int i = 4;
+		while (has_no_special_chars(synset_line[i+1])
+		       && has_no_special_chars(synset_line[i+2])) {
+		    if(synset_line[i].contains("_")) {
+			i += 2;
+			continue;
+		    }
+
+		    if(synset_line[i].contains("(p)"))
+			synset_line[i] = synset_line[i].replace("(p)", "");
+		    if(synset_line[i].contains("(a)"))
+			synset_line[i] = synset_line[i].replace("(a)", "");
+		    if(synset_line[i].contains("(ip)"))
+			synset_line[i] = synset_line[i].replace("(ip)", "");
+		    syns.add(synset_line[i]);
+		    i += 2;
+		}
+
+		if(syns.size() == 1)
+		    continue;
+
+		for (String syn: syns) {
+		    switch(s) {
+		    case "noun":
+			fill_synset(synset_noun, syn, syns);
+			break;
+
+		    case "verb":
+			fill_synset(synset_verb, syn, syns);
+			break;
+
+		    case "adj":
+			fill_synset(synset_adj, syn, syns);
+			break;
+
+		    case "adv":
+			fill_synset(synset_adv, syn, syns);
+			break;
+		    }
+		}
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.exit(-1);
+	}
     }
 
     /**
@@ -61,14 +231,26 @@ public class BooleanQueryWordnet {
      * @param wordnetDir the directory of the wordnet files
      */
     public void buildSynsets(Path wordnetDir) {
-	synset = new HashMap<>();
-	ArrayList<String> syns = new ArrayList<>();
-	syns.add("usa");
-	syns.add("u.s.a.");
-	syns.add("us");
-	synset.put("america", syns);
-    }
+        synset = new HashMap<>();
+        synset_noun = new HashMap<>();
+        synset_verb = new HashMap<>();
+        synset_adj = new HashMap<>();
+        synset_adv = new HashMap<>();
 
+        String[] pos = {"noun","verb","adj","adv"};
+
+        System.out.println("Building synsets...");
+
+        // Data files
+        for (String s: pos)
+	    parse_data_file(wordnetDir, s);
+
+        // Exc files
+        for (String s: pos)
+	    parse_exec_file(wordnetDir, s);
+
+	merge_synsets();
+    }
 
     /**
      * A method for reading the textual movie plot file and building a Lucene index.
@@ -80,101 +262,104 @@ public class BooleanQueryWordnet {
      * @param plotFile the textual movie plot file 'plot.list' to use
      */
     public void buildIndices(Path plotFile) {
-	StandardAnalyzer myAnalyzer = new StandardAnalyzer();
+        StandardAnalyzer myAnalyzer = new StandardAnalyzer();
 
-	try {
-	    Directory index = FSDirectory.open(Paths.get("../../index"));
-	    IndexWriterConfig config = new IndexWriterConfig(myAnalyzer);
-	    IndexWriter writer = new IndexWriter(index, config);
+	System.out.println("Parsing corpus...");
 
-	    DocumentParser parser = new DocumentParser();
-	    parser.build_doc_list(plotFile, writer);
-	    writer.commit();
-	    writer.close();
-	    IndexReader reader = DirectoryReader.open(index);
-	    indexSearcher = new IndexSearcher(reader);
-	    queryParser = new MultiFieldQueryParser(new String[]{"title", "plot", "type"}, new StandardAnalyzer());
-	} catch (IOException e) {
+        try {
+            Directory index = FSDirectory.open(Paths.get("../../index"));
+            IndexWriterConfig config = new IndexWriterConfig(myAnalyzer);
+            IndexWriter writer = new IndexWriter(index, config);
+
+            DocumentParser parser = new DocumentParser();
+            parser.build_doc_list(plotFile, writer);
+            writer.commit();
+            writer.close();
+            IndexReader reader = DirectoryReader.open(index);
+            indexSearcher = new IndexSearcher(reader);
+            queryParser = new MultiFieldQueryParser(new String[]{"title", "plot", "type"}, new StandardAnalyzer());
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
-	}
+        }
     }
 
     public ArrayList<String> splitQuery(String query) {
-	ArrayList<String> tokens = new ArrayList<>();
-	StringBuffer buffer = new StringBuffer();
-	for(int i=0; i<query.length(); i++) {
-	    char c = query.charAt(i);
-	    switch(c) {
-	    case ' ':
-	    case '(':
-	    case ')':
-		if(buffer.length() > 0) {
-		    tokens.add(buffer.toString());
-		    buffer = new StringBuffer();
-		}
-		if(c != ' ')
-		    tokens.add(Character.toString(c));
-		break;
-	    default:
-		buffer.append(c);
-		if (i == query.length() - 1)
-		    tokens.add(buffer.toString());
-	    }
-	}
-	return tokens;
+        ArrayList<String> tokens = new ArrayList<>();
+        StringBuffer buffer = new StringBuffer();
+        for(int i=0; i<query.length(); i++) {
+            char c = query.charAt(i);
+            switch(c) {
+                case ' ':
+                case '(':
+                case ')':
+                    if(buffer.length() > 0) {
+                        tokens.add(buffer.toString());
+                        buffer = new StringBuffer();
+                    }
+                    if(c != ' ')
+                        tokens.add(Character.toString(c));
+                    break;
+                default:
+                    buffer.append(c);
+                    if (i == query.length() - 1)
+                        tokens.add(buffer.toString());
+            }
+        }
+        return tokens;
     }
 
     public String expand(String token) {
-	String[] parts = token.split(":");
-	String field = parts[0];
-	String value = parts[1];
-	
-	ArrayList<String> synonyms = synset.get(value);
-	if(synonyms == null || field == "type")
-	    return token;
+        String[] parts = token.split(":");
+        String field = parts[0];
+        String value = parts[1];
 
-	String result = "(";
-	boolean first = true;
-	for(String synonym : synonyms) {
-	    if(first) {
-		first = false;
-	    } else {
-		result += " OR ";
-	    }
-	    result += field + ":" + synonym;
+        ArrayList<String> synonyms = synset.get(value);
+        if(synonyms == null) // || field.equals("type")
+            return token;
 
-	}
-	result += " " + token + ")";
-	return result;
+        String result = "(";
+        boolean first = true;
+        for(String synonym : synonyms) {
+            if(first) {
+                first = false;
+            } else {
+                result += " OR ";
+            }
+            result += field + ":" + synonym;
+	    //System.out.println("Synonym to " + value + ": " + synonym);
+
+        }
+        result += " OR " + token + ")";
+        return result;
     }
 
     public String expandQuery(String query) {
-	String result = "";
-	ArrayList<String> tokens = splitQuery(query);
+        String result = "";
+        ArrayList<String> tokens = splitQuery(query);
 
-	boolean first = true;
-	for(String token : tokens) {
-	    if(first) {
-		first = false;
-	    } else {
-		result += " ";
-	    }
-	    
-	    if(token.equals("(")
-	       || token.equals(")")
-	       || token.equals("AND")
-	       || token.equals("OR")) {
-		result += token;
-	    } else {
-		result += expand(token);
-	    }
-	}
-	// For testing, remove prints before deploy
-	System.out.println("Expansion: " + result);
-	
-	return result;
-	       
+        boolean first = true;
+        for(String token : tokens) {
+            if(first) {
+                first = false;
+            } else {
+                result += " ";
+            }
+
+            if(token.equals("(")
+                    || token.equals(")")
+                    || token.equals("AND")
+                    || token.equals("OR")) {
+                result += token;
+            } else {
+                result += expand(token);
+            }
+        }
+        // For testing, remove prints before deploy
+        System.out.println("Expansion: " + result);
+
+        return result;
+
     }
 
     /**
@@ -198,35 +383,35 @@ public class BooleanQueryWordnet {
      * lines (starting with "MV: ") of the documents matching the query
      */
     public Set<String> booleanQuery(String queryString) {
-	queryString = expandQuery(queryString);
-	
-	HashSet<String> result = new HashSet<>();
-	Query query = null;
-	TopDocs hits = null;
+        queryString = expandQuery(queryString);
 
-	try {
-	    query = queryParser.parse(queryString);
-	} catch (ParseException e) {
+        HashSet<String> result = new HashSet<>();
+        Query query = null;
+        TopDocs hits = null;
+
+        try {
+            query = queryParser.parse(queryString);
+        } catch (ParseException e) {
             e.printStackTrace();
             System.exit(-1);
-	}
+        }
 
-	try {
-	    hits = indexSearcher.search(query, 100);
-	} catch (IOException e) {
+        try {
+            hits = indexSearcher.search(query, 100);
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
-	}
+        }
 
-	for(ScoreDoc scoreDoc : hits.scoreDocs) {
-	    try {
-		Document doc = indexSearcher.doc(scoreDoc.doc);
-		result.add(doc.get("name"));
-	    } catch (IOException e) {
-		e.printStackTrace();
-		System.exit(-1);
-	    }
-	}
+        for(ScoreDoc scoreDoc : hits.scoreDocs) {
+            try {
+                Document doc = indexSearcher.doc(scoreDoc.doc);
+                result.add(doc.get("name"));
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
         return result;
     }
 
